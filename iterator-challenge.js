fe53ab5b48171d9b670769435a12dcb09281bcf2
awo.ali <awoali@hotmail.co.uk>
2022-06-01 10:24:30 +0100

    completed the first challenge

---
+++ iterator-challenge.js
@@ -0,0 +1,31 @@
+/* Capital names 🏰
+Write a function that:
+
+Takes an array of names as a parameter.
+It needs return a new array with each of the first letters of the names capitalized */
+
+
+nameArray = ['mark', 'rob', 'awo', 'lucy']
+
+const capitalizeFirstLetter = (array) => {
+    const capsOnly = nameArray.map(array =>{
+        return capsOnly.charAt(0).toUpperCae()
+    })
+   
+};
+
+console.log(nameArray);
+console.log(capitali);
+
+
+
+console.log('hello world');
+
+////
+
+/*const consonantsOnly = (array) => {
+    const removevowels = array.filter((letter)=>  letter !='a', 'e', 'i', 'o', 'u')
+    return (removevowels)
+};
+
+consonantsOnly = ['a', 'e', 'p', 'q'] */
